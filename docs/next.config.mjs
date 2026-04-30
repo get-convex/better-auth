ec39b97d24ce867844a4bf5f6e955416ae0c2928
Shawn Erquhart <shawn@erquh.art>
2026-04-29 21:03:29 -0400

    update fumadocs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -4,6 +4,9 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  turbopack: {
+    root: ".",
+  },
   basePath: "/better-auth",
   reactStrictMode: true,
   redirects: async () => {
